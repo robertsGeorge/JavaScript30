d254e8cb04b7e3b179636b3ae5224bb4b8b296cc
George Roberts <george.e.roberts@gmail.com>
2019-07-21 18:29:58 +0100

    Complete Custom HTML Video Player exercise

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,67 @@
+/* Get Our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle'); // the play button.
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+let mouseEngaged = false; // flag to use when changing range sliders.
+
+/* Build out functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+} // make the function and test it out in console before hooking it up to event listener
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate(e) {
+  if ( e.type !== 'change' && !mouseEngaged ) return;
+  /* because this.name is the same as the relevant property names on video, we can use square brackets. */
+  video[this.name] = this.value;  
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  console.log(e); // mouseEvent (so has data on where the mouse was when it was clicked, relative to event it was fired on?)
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+// use simple arrow function within event listener to update a global flag variable.
+// The function then uses the flag variable to decide whether to execute code or not.
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousedown', () => mouseEngaged = true));
+ranges.forEach(range => range.addEventListener('mouseup', () => mouseEngaged = false));
+ranges.forEach(range => range.addEventListener('mouseout', () => mouseEngaged = false));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
